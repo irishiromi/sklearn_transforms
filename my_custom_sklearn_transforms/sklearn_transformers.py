750e6492b44ccb329681a56fd12161f9874dfbf0
irishiromi <69720395+irishiromi@users.noreply.github.com>
2020-09-15 16:11:41 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,90 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+# Instanciando uma transformação DropColumns
+rm_columns = DropColumns(
+    columns=["NOME", "MATRICULA", "INGLES", "H_AULA_PRES", "TAREFAS_ONLINE", "FALTAS", "REPROVACOES_DE", "REPROVACOES_EM", "REPROVACOES_MF", "REPROVACOES_GO"]  # Essa transformação recebe como parâmetro uma lista com os nomes das colunas indesejadas
+)
+
+# Aplicando a transformação ``DropColumns`` ao conjunto de dados base
+rm_columns.fit(X=df_data_1)
+
+# Reconstruindo um DataFrame Pandas com o resultado da transformação
+df_data_2 = pd.DataFrame.from_records(
+    data=rm_columns.transform(
+        X=df_data_1
+    ),
+)
+
+#Drop linhas com notas acima de 10.
+novo_DF = df_data_1.loc[df_data_1['NOTA_MF']>10]
+
+# Criação de um objeto ``SimpleImputer``
+si = SimpleImputer(
+    missing_values=np.nan,  # os valores faltantes são do tipo ``np.nan`` (padrão Pandas)
+    strategy='constant',  # a estratégia escolhida é a alteração do valor faltante por uma constante
+    fill_value=5,  # a constante que será usada para preenchimento dos valores faltantes é um int64=0.
+    verbose=0,
+    copy=True
+)
+
+# Aplicamos o SimpleImputer ``si`` ao conjunto de dados df_data_2 (resultado da primeira transformação)
+si.fit(X=df_data_2)
+
+# Reconstrução de um novo DataFrame Pandas com o conjunto imputado (df_data_3)
+df_data_3 = pd.DataFrame.from_records(
+    data=si.transform(
+        X=df_data_2
+    ),  # o resultado SimpleImputer.transform(<<pandas dataframe>>) é lista de listas
+    columns=df_data_2.columns  # as colunas originais devem ser conservadas nessa transformação
+)
+
+#Calculamos a média de exatas
+df_data_3['MEDIA_EXATAS'] = (df_data_3['NOTA_MF'] + df_data_3['NOTA_GO']) / 2
+df_data_3.head()
+
+
+#Calculamos a média de humanas
+df_data_3['MEDIA_HUMANAS'] = (df_data_3['NOTA_EM'] + df_data_3['NOTA_DE']) / 2
+df_data_3.head()
+
+# Criação de uma árvore de decisão com a biblioteca ``scikit-learn``:
+
+
+# Oversample and plot imbalanced dataset with ADASYN
+from collections import Counter
+from sklearn.datasets import make_classification
+from imblearn.over_sampling import ADASYN
+from matplotlib import pyplot
+from numpy import where
+from numpy import loadtxt
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+# define dataset
+X, y = make_classification(n_samples=10000, n_features=2, n_redundant=0,
+	n_clusters_per_class=1, weights=[0.99], flip_y=0, random_state=1)
+# summarize class distribution
+counter = Counter(y)
+print(counter)
+# transform the dataset
+oversample = ADASYN()
+X, y = oversample.fit_resample(X, y)
+# summarize the new class distribution
+counter = Counter(y)
+print(counter)
+# scatter plot of examples by class label
+for label, _ in counter.items():
+	row_ix = where(y == label)[0]
+	pyplot.scatter(X[row_ix, 0], X[row_ix, 1], label=str(label))
+pyplot.legend()
+pyplot.show()
+
+#Aplicamos o modelo escolhido
+from imblearn.over_sampling import SMOTE, ADASYN
+X_resampled, y_resampled = SMOTE(). fit_resample(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=1)
+
+model = XGBClassifier()
+model.fit(X_train, y_train)
